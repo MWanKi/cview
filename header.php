70be834086b1e961ab8dd3dc33fa66d939f77f9c
Wanki <www@cside.co.kr>
2016-09-01 12:06:44 +0900

    씨뷰 사이드바 완성
    - 사이드바 완성했습니다.

---
+++ header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>C.VIEW</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/notosanskr.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="css/common.css">
+	<script src="js/jquery.3.1.0.js"></script>
+</head>
+<body>
+	<div class="wrap-content">
+
+		<!-- header -->
+		<div class="wrap-header">
+			<a href="#" class="btn-menu">
+				<span class="bar1"></span>
+				<span class="bar2"></span>
+				<span class="bar3"></span>
+			</a>
+			<h2>오늘함</h2>
+		</div>
+		<!-- header -->
+
+		<!-- sidebar -->
+		<div class="wrap-sidebar">
+			<div class="box-top">
+				<div class="box-profile">
+					<img src="/images/profile.png" alt="">
+				</div>
+				<p class="txt-name">C.SIDE Inc.</p>
+				<a href="#" class="btn-setting">
+					<img src="/images/setting.png" alt="">
+				</a>
+			</div>
+			<ul class="ul-sidebar ul-topnav">
+				<li class="li-nav li-all li-on">
+					<a href="#">
+						전체함
+						<span class="num-count">22</span>
+					</a>
+				</li>
+				<li class="li-nav li-today">
+					<a href="#">
+						오늘
+						<span class="num-count">10</span>
+					</a>
+				</li>
+				<li class="li-nav li-tomorrow">
+					<a href="#">
+						내일
+						<span class="num-count">4</span>
+					</a>
+				</li>
+				<li class="li-nav li-nextweek">
+					<a href="#">
+						다음 7일
+						<span class="num-count">8</span>
+					</a>
+				</li>
+				<li class="li-nav li-calendar">
+					<a href="#">
+						캘린더
+						<span class="num-count"></span>
+					</a>
+				</li>
+			</ul>
+			<ul class="ul-sidebar ul-botnav">
+				<li class="li-nav li-addgroup">
+					<a href="#">
+						그룹함 추가
+					</a>
+				</li>
+				<li class="li-nav li-managegroup">
+					<a href="#">
+						그룹함 관리
+					</a>
+				</li>
+			</ul>
+		</div>
+		<!-- sidebar -->
