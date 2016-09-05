4a520183507d7c738fd5751354d17ba0d7fe6c24
Wanki <www@cside.co.kr>
2016-09-05 10:28:47 +0900

    주별 달력 오늘날짜 오류 수정
    - 이번달 날짜를 받아오도록 수정했습니다.

--- _calendar-week.php
+++ _calendar-week.php
@@ -5,6 +5,7 @@ $month = isset($_GET['month']) ? $_GET['month'] : date('m');
 $day = isset($_GET['day']) ? $_GET['day'] : date('d'); 
 
 $w = date('w',mktime(0,0,0,$month,$day,$year)); 
+$data_month = date('m',mktime(0,0,0,$month,$day,$year)); 
 $m = $day-($w+7)%7;
 
 $data = '<li class="li-week add" data-month='.(int)date("m",strtotime($day-date('d')." day")).'>';
@@ -15,7 +16,7 @@ for ($i=0; $i < 7; $i++) {
     $data_day = (int)date('d',mktime(0,0,0,$month,$m+$i,$year));
 
     // 현재달과 지난달 구분하기
-    if ($month == date('m') && $data_day == date('d')) {
+    if ($month == $data_month && $data_day == date('d')) {
         $data .= "  <td class='today'><span>$data_day</span></td>";
     } else {
         $data .= "  <td><span>$data_day</span></td>";
@@ -25,7 +26,6 @@ for ($i=0; $i < 7; $i++) {
 $data .= '      </tr>';
 $data .= '  </table>';
 $data .= '</li>';
-
 echo $data;
 
 
